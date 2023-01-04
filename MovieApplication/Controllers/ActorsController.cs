a1d8de8d0de5b264048f7c45f990609f679d0c83
Arun <karanam.arun77@gmail.com>
2023-01-04 16:10:06 +0530

    Added Controllers

---
+++ MovieApplication/Controllers/ActorsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApplication.Data;
+
+namespace MovieApplication.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDBContext _context;
+        public ActorsController(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var data= _context.Actors.ToList();
+            return View();
+        }
+    }
+}
---
+++ MovieApplication/Controllers/ProducersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieApplication.Data;
+
+namespace MovieApplication.Controllers
+{
+    public class ProducersController : Controller
+    {
+        private readonly AppDBContext _context;
+        public ProducersController(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        public async  Task<IActionResult> Index()
+        {
+            var allProducers= await _context.Producers.ToListAsync();
+            return View();
+        }
+    }
+}
